d8e7d3c9357963a5f6870e8da5413e5a04a671c3
fnngj <fnngj@126.com>
2018-09-28 23:17:09 +0800

    完成项目的登录退出

--- test_platform/test_platform/urls.py
+++ test_platform/test_platform/urls.py
@@ -21,5 +21,8 @@ from user_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path("login_action/", views.login_action)
+    path("accounts/login/", views.index),
+    path("login_action/", views.login_action),
+    path("logout/", views.logout),
+    path("project_manage/", views.project_manage),
 ]
--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
-
+from django.contrib.auth.decorators import login_required
 
 # 首页
 def index(request):
@@ -19,13 +19,25 @@ def login_action(request):
                           {"error": "用户名或者密码为空"}
                          )
         else:
-            user = auth.authenticate(
-                username=username, password=password)
-            print(user)
-            print(type(user))
+            user = auth.authenticate(username=username, password=password)  # 验证用户是不是存在
+            
             if user is not None:
-                auth.login(request, user)  # 验证登录
-                return render(request, "project_manage.html")
+                auth.login(request, user) #记录用户登录状态
+                request.session['user1'] = username
+                return HttpResponseRedirect('/project_manage/')
             else:
                 return render(request, "index.html",
                                         {"error": "用户名或者密码错误"})
+
+
+@login_required #判断用户是否登录
+def project_manage(request):
+    username = request.session.get('user1', '')  # 读取浏览器 session
+    return render(request, "project_manage.html", {"user": username})
+
+
+# 退出登录
+def logout(request):
+    auth.logout(request)  # 清楚用户登录状态
+    response = HttpResponseRedirect('/')
+    return response
