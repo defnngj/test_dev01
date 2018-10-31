84b9eb5b82d2b66593d54aec83c61429756adb4e
defnngj <353332934@qq.com>
2018-10-31 15:04:27 +0800

    增加项目创建用例

--- test_platform/project_app/tests.py
+++ test_platform/project_app/tests.py
@@ -21,3 +21,24 @@ class ProjectMangeTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn("xx项目", project_html)
         self.assertIn("xx描述", project_html)
+
+
+class ProjectAddTest(TestCase):
+    ''' 添加项目 '''
+
+    def setUp(self):
+        User.objects.create_user('admin', 'admin@mail.com', 'admin123456')
+        Project.objects.create(name="xx项目", describe="xx描述")
+        login_user = {'username': 'admin', 'password': 'admin123456'}
+        self.client.post('/login_action/', data=login_user)  # 预先登录
+
+    def test_project_add_test(self):
+        ''' 添加成功，做302跳转 '''
+        data = {"name": "test111", "describe": "test111", "status": True}
+        response = self.client.post('/manage/add_project/', data=data)
+        project_html = response.content.decode('utf-8')
+        print(project_html)
+        self.assertEqual(response.status_code, 302)
+        
+
+# 测试编辑接口的话，需要先创建一条已经存在的数据，再对该数据进行提交，注意ID一致，否则就是创建了。
