6b1af999122480938690cd6b4b8513b055820fb3
defnngj <353332934@qq.com>
2018-11-08 19:28:24 +0800

    修改目录结构

--- test_platform/interface_app/urls.py
+++ test_platform/interface_app/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from interface_app import views
+from interface_app.views import testcase_views
 
 
 urlpatterns = [
     # guest system interface:
     # ex : /intereface/case_manage/
     # 用例管理
-    path('case_manage/', views.case_manage),
-    path('debug/', views.debug),
-    path('api_debug/', views.api_debug),
-    path('save_case/', views.save_case)
+    path('case_manage/', testcase_views.case_manage),
+    path('debug/', testcase_views.debug),
+    path('api_debug/', testcase_views.api_debug),
+    path('save_case/', testcase_views.save_case),
 ]
 
 
